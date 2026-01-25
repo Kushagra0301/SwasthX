20ad62b9f6292b6908e46d7b929d21bc13c97735
Kushagra Sharma <kushagrasharmaudr@gmail.com>
2026-01-25 15:23:24 +0530

    Initial production version 4

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,5 @@
-// lib/prisma.ts
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-declare global {
-  // allow global var in dev to avoid multiple instances in HMR
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
-
-const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+const prisma = new PrismaClient();
 
 export default prisma;
